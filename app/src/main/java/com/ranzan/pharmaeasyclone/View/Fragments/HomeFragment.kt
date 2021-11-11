b5348ea493394977681f407ee38d5c5529226060
RANZN <aryaprakashranjan@gmail.com>
2021-11-11 14:26:36 +0530

    asdfsa

--- app/src/main/java/com/ranzan/pharmaeasyclone/View/Fragments/HomeFragment.kt
+++ app/src/main/java/com/ranzan/pharmaeasyclone/View/Fragments/HomeFragment.kt
@@ -10,6 +10,6 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
+
 }
